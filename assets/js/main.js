59013f86faa2044aae60e8cc501aac812d22de7d
BlackPlay97 <quocdai01vn@gmail.com>
2023-02-26 21:54:12 +0700

    not repo

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+$(document).ready(function () {
+  $(".owl-carousel-feedback").owlCarousel();
+});
+$(".owl-carousel-feedback").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
